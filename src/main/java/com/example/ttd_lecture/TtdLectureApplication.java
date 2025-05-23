828d214a71b6062464135a756774e25f1a8eafbe
yunyechan <dpcks9893@naver.com>
2025-05-23 09:33:17 +0900

    Initial commit

---
+++ src/main/java/com/example/ttd_lecture/TtdLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.ttd_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtdLectureApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TtdLectureApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/ttd_lecture/TtdLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ttd_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtdLectureApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

134f57875f377eee22ae79dcdfaa4414e0506aab
yunyechan <dpcks9893@naver.com>
2025-05-27 12:56:39 +0900

    [Study] 3일차 학습완료

--- js_test/4_단위테스트/index.js
+++ js_test/4_단위테스트/index.js
@@ -1,5 +1,19 @@
-function refineText(s) {
-  return s.replace("    ", " ").replace("  ", " ")
+function refineText(s, options) {
+  s = s.replace("       ", " ")
+    .replace("    ", " ")
+    .replace("\t", " ")
+    .replace("   ", " ")
+    .replace("  ", " ")
+    .replace("purist", "******")
+    .replace("mockist", "******")
+
+  if (options) {
+    for(const bannedWord of options.bannedWords) {
+      s = s.replace(bannedWord, "*".repeat(bannedWord.length))
+    }
+  }
+
+  return s;
 }
 
 module.exports = refineText;
--- js_test/4_단위테스트/index.test.js
+++ js_test/4_단위테스트/index.test.js
@@ -1,19 +1,48 @@
-const sut = require("./index");
+const { faker } = require('@faker-js/faker');
+const sut = require('./index');
 
 // 파라미터 라이즈 테스트
 test.each`
   source | expected
-  ${"hello  world"} | ${"hello world"}
-  ${"hello   world"} | ${"hello world"}
-  ${"hello    world"} | ${"hello world"}
+  ${'hello  world'} | ${'hello world'}
+  ${'hello   world'} | ${'hello world'}
+  ${'hello    world'} | ${'hello world'}
+  ${'hello     world'} | ${'hello world'}
+  ${'hello      world'} | ${'hello world'}
+  ${'hello       world'} | ${'hello world'}
+  ${'hello        world'} | ${'hello world'}
 `(`sut transforms "$source" to "$expected"`, ({ source, expected }) => {
-  const actual = sut(source)
-  expect(actual).toBe(expected)
-})
+  const actual = sut(source);
+  expect(actual).toBe(expected);
+});
 
-test('sut correctly works', () => {
-  for (const source of ['hello  world', 'hello   world', 'hello    world']) {
-    const actual = sut(source)
-    expect(actual).toBe("hello world")
-  }
-})
+test.each`
+  source | expected
+  ${'hello\tworld'} | ${'hello world'}
+  ${'hello\t world'} | ${'hello world'}
+`('sut transforms "$source" that contains tab charactor to "$expected"', ({ source, expected }) => {
+  const actual = sut(source);
+  expect(actual).toBe(expected);
+});
+
+test.each`
+  source | bannedWords | expected
+  ${'hello mockist'} | ${['mockist', 'purist']} | ${'hello ******'}
+  ${'hello purist'} | ${['mockist', 'purist']} | ${'hello ******'}
+  `('sut transforms "$source" to $"expected"',
+  ({ source, bannedWords, expected }) => {
+    const actual = sut(source, { bannedWords });
+    expect(actual).toBe(expected);
+  });
+
+
+describe('given bannerd word', () => {
+  const bannedWord = faker.lorem.word();
+  const source = 'hello ' + bannedWord;
+  const expected = 'hello ' + '*'.repeat(bannedWord.length);
+
+  test(`${bannedWord} when invoke sut then it returns`, () => {
+    const actual = sut(source, { 'bannedWords': [bannedWord] });
+    expect(actual).toBe(expected);
+  });
+});
